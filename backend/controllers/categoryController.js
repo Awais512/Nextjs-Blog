ddef71d4b0a56be115a5bcdb2a0f6b9ac70fe2b4
awais512 <raza.awais25@gmail.com>
2020-12-28 17:09:25 +0500

    Categories Crud Operations

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -2,6 +2,9 @@ const Category = require('../models/categoryModel');
 const slugify = require('slugify');
 const asyncHandler = require('express-async-handler');
 
+//@desc     Create Category
+//@route    POST /api/categories/create
+//@access   Private/Admin
 exports.createCategory = asyncHandler(async (req, res) => {
   const name = req.body.name;
   const slug = slugify(name).toLowerCase();
@@ -15,4 +18,38 @@ exports.createCategory = asyncHandler(async (req, res) => {
 
   const category = await new Category({ name, slug }).save();
   res.json(category);
+});
+
+//@desc     Create Category
+//@route    POST /api/categories/create
+//@access   Private/Admin
+exports.getCategories = asyncHandler(async (req, res) => {
+  const categories = await Category.find({});
+  res.json({ count: categories.length, categories });
+});
+
+//@desc     Delete Category
+//@route    POST /api/categories/:slug
+//@access   Private/Admin
+exports.getCategory = asyncHandler(async (req, res) => {
+  const slug = req.params.slug.toLowerCase();
+  const category = await Category.findOne({ slug });
+  if (!category) {
+    res.status(400);
+    throw new Error('Category does not exist');
+  }
+  res.json({ category });
+});
+//@desc     Delete Category
+//@route    POST /api/categories/:slug
+//@access   Private/Admin
+exports.deleteCategory = asyncHandler(async (req, res) => {
+  const slug = req.params.slug.toLowerCase();
+  let category = await Category.findOne({ slug });
+  if (!category) {
+    res.status(400);
+    throw new Error('Category does not exist');
+  }
+  await category.remove();
+  res.json({ message: 'Category deleted successfully', data: {} });
 });
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { requireSignin } = require('../controllers/authController');
-const { createCategory } = require('../controllers/categoryController');
+const {
+  createCategory,
+  getCategories,
+  getCategory,
+  deleteCategory,
+} = require('../controllers/categoryController');
 const { authenticated, isAdmin } = require('../middlewares/authMiddleware');
 
 router.post('/create', requireSignin, isAdmin, createCategory);
+router.get('/', getCategories);
+router.get('/:slug', getCategory);
+router.delete('/:slug', requireSignin, isAdmin, deleteCategory);
 
 module.exports = router;
