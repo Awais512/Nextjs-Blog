cc392bf03d333eab763e9ec66dec0e4b2b335fd7
awais512 <raza.awais25@gmail.com>
2020-12-28 20:45:18 +0500

    CSS using static file and next css

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import '../node_modules/nprogress/nprogress.css';
 
 class MyDocument extends Document {
   render() {
@@ -14,10 +15,8 @@ class MyDocument extends Document {
           <link
             rel='stylesheet'
             href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
-            integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=='
-            crossorigin='anonymous'
           />
-          <link rel='stylesheet' href='/static/css/styles.css' />
+          {/* <link rel='stylesheet' href='/static/css/styles.css' /> */}
         </Head>
         <body>
           <Main />
